50e596086f57d82c9752ea651117aa0dd6791e2d
Krasimir Nedev <krasi.nedew@gmail.com>
2016-10-31 17:33:05 +0200

    Swapped forward and backward camera logic

--- SimpleWars/Camera/Camera.cs
+++ SimpleWars/Camera/Camera.cs
@@ -28,6 +28,7 @@
             {
                 var lookAtVector = LookAt;
                 var rotationMatrix = Matrix.CreateRotationZ(angle);
+
                 lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
                 lookAtVector += this.position;
 
@@ -55,18 +56,6 @@
             {
                 if (yRatio < 0.5f)
                 {
-                    var backwardVector = new Vector3(0, 1, 0);
-
-                    var rotationMatrix = Matrix.CreateRotationZ(angle);
-                    backwardVector = Vector3.Transform(backwardVector, rotationMatrix);
-
-                    const float UnitsPerSecond = 5;
-
-                    this.position += backwardVector * UnitsPerSecond *
-                        (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-                else
-                {
                     var forwardVector = new Vector3(0, -1, 0);
 
                     var rotationMatrix = Matrix.CreateRotationZ(angle);
@@ -75,6 +64,18 @@
                     const float UnitsPerSecond = 5;
 
                     this.position += forwardVector * UnitsPerSecond * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else
+                {
+                    var backwardVector = new Vector3(0, 1, 0);
+
+                    var rotationMatrix = Matrix.CreateRotationZ(angle);
+                    backwardVector = Vector3.Transform(backwardVector, rotationMatrix);
+
+                    const float UnitsPerSecond = 5;
+
+                    this.position += backwardVector * UnitsPerSecond *
+                        (float)gameTime.ElapsedGameTime.TotalSeconds;                
                 }
             }
             else
