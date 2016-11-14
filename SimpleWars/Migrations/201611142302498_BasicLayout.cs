73f96f15fc130cb940b528ad179c008faee90a80
Krasimir Nedev <krasi.nedew@gmail.com>
2016-11-15 01:05:24 +0200

    Added basic (first working db layout) migration.

---
+++ SimpleWars/Migrations/201611142302498_BasicLayout.cs
@@ -0,0 +1,16 @@
+namespace SimpleWars.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BasicLayout : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
