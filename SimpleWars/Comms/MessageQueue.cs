dde290bc04c749833da90dcc00710bb92ddc8d80
Krasimir Nedev <krasi.nedew@gmail.com>
2016-12-08 22:54:08 +0200

    Added message queue.

---
+++ SimpleWars/Comms/MessageQueue.cs
@@ -0,0 +1,36 @@
+﻿namespace SimpleWars.Comms
+{
+    using System.Collections.Concurrent;
+    using System.Threading;
+
+    using SimpleWars.ModelDTOs;
+
+    public class MessageQueue
+    {
+        private readonly BlockingCollection<Message> queue;
+
+        public MessageQueue()
+        {
+            this.queue = new BlockingCollection<Message>();
+        }
+
+        public void Enqueue(Message message)
+        {
+            this.queue.Add(message);
+        }
+
+        public Message Dequeue()
+        {
+            Message msg;
+            bool took = this.queue.TryTake(out msg);
+            return took ? msg : null;
+        }
+
+        public Message TryDequeue()
+        {
+            Message msg;
+            bool took = this.queue.TryTake(out msg, Timeout.Infinite);
+            return took ? msg : null;
+        }
+    }
+}
